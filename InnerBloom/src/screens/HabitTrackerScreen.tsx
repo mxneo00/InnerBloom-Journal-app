2a5bc4441d1c3db3ec5b425da85a84825603b976
Katellyn Hyker <llyn00@yahoo.com>
2026-01-26 11:31:04 -0500

    Begin habit tracker layout planning

--- InnerBloom/src/screens/HabitTrackerScreen.tsx
+++ InnerBloom/src/screens/HabitTrackerScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
 
 // SRC Imports
 import { styles } from '../styles/commonStyles';
@@ -8,9 +8,22 @@ import { habitStyles } from '../styles/habitTrackerScreenStyles';
 export default function HabitTrackerScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Habit Tracking</Text>
+      {/* Weekly Header Section */}
+      <View style={habitStyles.weeklyHeader}>
+        <Text style={habitStyles.weeklyHeaderText}>This Weeks Habits</Text>
+        <Text style={habitStyles.daterange}>Date1 - Date2</Text>
+        <Text style={habitStyles.progress}>Progress: 12/21</Text>
+        <Pressable style={habitStyles.addHabitButton}>
+          <Text style={habitStyles.addHabitButtonText}>+ Add Habit</Text>
+        </Pressable>
+      </View>
+      {/* Daily Habit Section */}
       <View style={styles.card}>
-        <Text style={styles.text}>Feature coming soon!</Text>
+        <Text style={styles.text}>Daily Habits</Text>
+      </View>
+      {/* Weely Habit Section */}
+      <View style={styles.card}>
+        <Text style={styles.text}>Weekly Habits</Text>
       </View>
     </View>
   );
--- InnerBloom/src/styles/habitTrackerScreenStyles.ts
+++ InnerBloom/src/styles/habitTrackerScreenStyles.ts
@@ -1,5 +1,22 @@
 import { StyleSheet } from 'react-native';
 
 export const habitStyles = StyleSheet.create({
-   
+   weeklyHeader: {
+        
+    },
+    weeklyHeaderText: {
+        
+    },
+    daterange: {
+        
+    },
+    progress: {
+        
+    },
+    addHabitButton: {
+        
+    },
+    addHabitButtonText: {
+        
+    },
 });
