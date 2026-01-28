7a21f93cfdeae2e1b132970547bb83d8aaa8932e
Katellyn Hyker <llyn00@yahoo.com>
2026-01-28 14:09:32 -0500

    add delete button to ViewEntryScreen

--- InnerBloom/src/styles/commonStyles.ts
+++ InnerBloom/src/styles/commonStyles.ts
@@ -61,6 +61,21 @@ export const styles = StyleSheet.create({
   buttonText: {
     fontSize: 24,
   },
+  buttonPressed: {
+    opacity: 0.7,
+  },
+  deleteButton: {
+    backgroundColor: colors.primaryDark,
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+    alignItems: 'center',
+  },
+  deleteButtonText: {
+    color: colors.textSecondary,
+    fontSize: 16,
+    fontWeight: '600',
+  },
   saveButton: {
     backgroundColor: colors.primary,
     paddingVertical: 12,
@@ -72,9 +87,6 @@ export const styles = StyleSheet.create({
     color: colors.textPrimary,
     fontSize: 16,
     fontWeight: '600',
-  },
-  buttonPressed: {
-    opacity: 0.7,
   },
 
   /* ---------- Mood Picker ---------- */
