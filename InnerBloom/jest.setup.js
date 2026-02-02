6111cb8da8e2a3bc82c56c92894ffe6c7ac8036e
Katellyn Hyker <llyn00@yahoo.com>
2026-02-02 10:00:42 -0500

    Set up jest

---
+++ InnerBloom/jest.setup.js
@@ -0,0 +1,9 @@
+// Jest Setup File
+// This file runs before each test file
+
+// Mock react-native-reanimated
+jest.mock('react-native-reanimated', () => {
+  const Reanimated = require('react-native-reanimated/mock');
+  Reanimated.default.call = () => {};
+  return Reanimated;
+});
